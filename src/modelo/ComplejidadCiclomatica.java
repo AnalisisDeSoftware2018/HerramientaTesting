dd1aa3828b3ca1748480234281a3e8d7d0e70b80
eliasgarcia <garcia.elias.emg@gmail.com>
2018-10-19 15:58:41 -0300

    se corrigió el mal calculo de complejidad ciclomatica

--- src/modelo/ComplejidadCiclomatica.java
+++ src/modelo/ComplejidadCiclomatica.java
@@ -3,14 +3,19 @@ package modelo;
 public class ComplejidadCiclomatica {
 
 	public static int obtener(String codigo) {
-		int c = 0;
-		String[] lineas = codigo.split("\\n");
+		int c = 1, a;
+		String[] lineas = codigo.split("\n");
 		for (int i = 0; i < lineas.length; i++) {
-			c += (Parser.ocurrenciasPorLinea(lineas[i], "&&") + Parser.ocurrenciasPorLinea(lineas[i], "||"));
-			if (lineas[i].contains("if") || lineas[i].contains("while") || lineas[i].contains("for")
-					|| lineas[i].contains("case") || lineas[i].contains("catch")) {
-				c++;
+			a = (Parser.ocurrenciasPorLinea(lineas[i], "&&") + Parser.ocurrenciasPorLinea(lineas[i], "||"))+1;
+			if(a==1) {
+				a--;
 			}
+			if (a==0 && (lineas[i].contains("if") || lineas[i].contains("while") || lineas[i].contains("for(") || 
+					lineas[i].contains("for (")
+					|| lineas[i].contains("case") || lineas[i].contains("catch"))) {
+				a=1;
+			}
+			c+=a;
 		}
 
 		return c;
